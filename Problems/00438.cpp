91ee81f69a31f774e56de30cf9f0c279b5ba4cd4
Amit Kumar <offamitkumar@gmail.com>
2020-07-30 09:51:16 +0530

    find all anagrams, medium

--- Problems/00438.cpp
+++ Problems/00438.cpp
@@ -1,28 +1,17 @@
 class Solution {
-public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int>ans;
-        if(s.empty() or p.empty() or s.size() < p.size()){
+    public:
+        vector<int> findAnagrams(string s, string p) {
+            std::map<char, int> f;
+            for(auto&itr:p){
+                f[itr]++;
+            }
+            std::vector<int> ans;
+            int left{} , right{} , counter = p.size(); 
+            while(right <(int)s.size()){
+                if(f[s[right++]]-- >= 1) counter --; 
+                if(counter==0)ans.push_back(left); 
+                if(right-left == (int)p.size() && f[s[left++]]++>=0)counter++;
+            }
             return ans;
         }
-        int right = 0 , left = 0;
-        size_t n = p.size();
-        array<int , 26>frequency;
-        fill(frequency.begin() , frequency.end() , 0);
-        for(auto&itr:p){
-            ++frequency[itr-'a'];
-        }
-        size_t m = s.size();
-        const size_t windowSize = p.size();
-        while(right < m){
-            if(frequency[s[right++]-'a']-->=1)n--; // found a matching word
-            if(0 == n){ // matched complete word
-                ans.emplace_back(left);
-            }
-            if(right-left==windowSize && frequency[s[left++]-'a']++>=0){
-                ++n;
-            }
-        }
-        return ans;
-    }
 };
