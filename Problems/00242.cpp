7f12daef04a13922f36bd2bc76c62e275c83e07a
Amit Kumar <offamitkumar@gmail.com>
2021-08-16 18:38:32 +0530

    easy

---
+++ Problems/00242.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) {
+            return false; 
+        }
+        array<int , 26> f = {0}; 
+        for (int i=0; i<int(s.length()); ++i) {
+            f[s[i]-'a']++;
+            f[t[i]-'a']--;
+        }
+        return all_if(f.begin(),f.end() , [](bool x) {return x == true;}); 
+    }
+};
