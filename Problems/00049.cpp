6eff45f948a471c04bb7e5c7ea465619b8b86232
Amit Kumar <offamitkumar@gmail.com>
2021-08-16 18:27:28 +0530

    updated

--- Problems/00049.cpp
+++ Problems/00049.cpp
@@ -7,12 +7,10 @@ public:
             sort(s.begin() , s.end());
             mp[s].push_back(itr);
         }
-        vector<vector<string>>v(mp.size());
+        vector<vector<string>>v;
         int i = 0;
-        for(auto&itr:mp){
-            for(auto&k:itr.second){
-                v[i].push_back(k);
-            }
+        for(auto itr:mp){
+            v.push_back(itr.second);
             i++;
         }
         return v;
