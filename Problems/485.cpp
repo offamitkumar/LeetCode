0f22df43462ba87292bce97e4cc1cab7ca6299cc
Amit Kumar <offamitkumar@gmail.com>
2021-09-21 17:59:58 +0530

    adhoc

--- Problems/485.cpp
+++ Problems/485.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int max_{} , t{}; 
-        for(int i=0; i<(int)nums.size(); ++i){
-            if(nums[i]){
-                t++; 
-            }else{
-                if(t > max_){
-                    max_ = t;
-                }
-                t = 0;
-            }
+        int ans{} , temp{}; 
+        for (auto&itr:nums) {
+            temp+=(itr); 
+            ans = max(ans , temp); 
+            if(!itr)temp=0;
         }
-        if(t > max_){
-            max_ = t;
-        }
-        return max_;
+        return ans;
     }
 };
