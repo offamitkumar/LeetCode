907044b25350c6306d5af88ca98b11700e1a888a
Amit Kumar <offamitkumar@gmail.com>
2021-10-04 12:31:37 +0530

    adhoc

---
+++ Problems/27. Remove Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j = (int)nums.size()-1;
+        int counter{};
+        for(int i=0;i<(int)nums.size() && i<=j;++i){
+            while(j>=0 && nums[j] == val){
+                --j;
+            }
+            if(i<=j && j>=0 && nums[i] == val){
+                swap(nums[i] , nums[j]);
+                --j;
+                ++counter;
+            }
+        }
+        return max(0 , j+1);
+    }
+};
