9b97b8c083ece971db8abcb922271976aaa05e97
offamitkumar <offamitkumar@gmail.com>
2021-10-15 13:54:16 +0530

    space optimized dp

--- Problems/121. Best Time to Buy and Sell Stock.cpp
+++ Problems/121. Best Time to Buy and Sell Stock.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         const int &n = prices.size(); 
-        vector<int>dp(n); 
+        int best = 0;
         int mxd = 0 - prices[0]; 
         for (int i=1; i<n; ++i) {
-            dp[i] = max(dp[i-1] , prices[i] + mxd); 
+            best = max(best , prices[i] + mxd); 
             mxd = max(mxd ,-prices[i]);
         }
-        return dp[n-1];
+        return best;
     }
 };
+
