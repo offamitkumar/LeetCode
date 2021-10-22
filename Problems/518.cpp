2f2570ab0569ac84ae5e40f7e62bb81ed791ae49
offamitkumar <offamitkumar@gmail.com>
2021-10-22 19:13:16 +0530

    linear dp

--- Problems/518.cpp
+++ Problems/518.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
-        dp[0][0] = 1;
-        for (int i = 1; i <= coins.size(); ++i) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= amount; ++j) {
-                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+        vector<int>dp(amount+1); 
+        dp[0] =1; 
+        sort(coins.begin(), coins.end()); 
+        for (int i=0; i<coins.size(); ++i) {
+            for (int j=coins[i]; j<=amount; ++j) {
+                dp[j] += dp[j-coins[i]]; 
             }
         }
-        return dp[coins.size()][amount];
+        return dp[amount];
     }
 };
