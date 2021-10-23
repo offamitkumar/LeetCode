dc46608847f227b4f271aed13c23e9e58d42b572
offamitkumar <offamitkumar@gmail.com>
2021-10-23 21:28:50 +0530

    binary search

--- Problems/153. Find Minimum in Rotated Sorted Array.cpp
+++ Problems/153. Find Minimum in Rotated Sorted Array.cpp
@@ -4,9 +4,6 @@ public:
         int low = 0 , high = nums.size() -1; 
         while (low < high){
             int mid = (high + low) / 2; 
-            if (nums[low] < nums[high]) {
-                return nums[low];
-            }
             if (nums[mid] > nums[high]) {
                 low = mid+1;
             } else {
