b6fb7e1f016de56cd6b2d3a157a2030fb152fa6c
offamitkumar <offamitkumar@gmail.com>
2021-11-04 22:19:10 +0530

    adhoc, hard

--- Problems/41.cpp
+++ Problems/41.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        for(auto&itr:nums) {
-            if (itr>=n or itr<0) {
-                itr = n+100;
+        int n = nums.size(); 
+        if(none_of(nums.begin(), nums.end(), [](int x) { return x == 1;})){
+            return 1; 
+        }
+        for (auto &itr: nums) {
+            if (itr <= 0 || itr > n) {
+                itr = 1; 
             }
         }
-        for(int i=0; i<n; ++i) {
-            if (nums.at(i) < n) {
-                int x = abs(nums.at(i)); 
-                nums.at(x) = -abs(nums.at(x));
+        for (auto&itr:nums) {
+            int x= abs(itr); 
+            if (nums[x-1]>0) nums[x-1]*=-1; 
+        }
+        for (int i=0; i<n; ++i) {
+            if (nums[i] > 0) {
+                return i+1; 
             }
         }
-        for(int i=0; i<n; ++i) {
-            if (nums.at(i)>0){
-                return i+1;
-            }
-        }
-        return n;
+        return n+1; 
     }
 };
