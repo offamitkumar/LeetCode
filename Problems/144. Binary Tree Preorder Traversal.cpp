31f6eaa947f23eafd56a6e0bb4e533e6c685bdec
offamitkumar <offamitkumar@gmail.com>
2021-11-16 00:08:08 +0530

    dfs

---
+++ Problems/144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int>ans; 
+    void dfs(TreeNode*root) {
+        if (root==nullptr) return ;
+        ans.push_back(root->val); 
+        dfs(root->left); 
+        dfs(root->right);
+    }
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        dfs(root); 
+        return ans; 
+    }
+};
