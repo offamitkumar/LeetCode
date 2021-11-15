7d48145608bab6a523d0b003ce85f856e46762b2
offamitkumar <offamitkumar@gmail.com>
2021-11-15 23:46:39 +0530

    dp,longest common substing

--- Problems/718. Maximum Length of Repeated Subarray.cpp
+++ Problems/718. Maximum Length of Repeated Subarray.cpp
@@ -4,25 +4,11 @@ public:
         const int&n = nums1.size(); 
         const int &m = nums2.size(); 
         int ans{}; 
-        vector<vector<int>>dp(n , vector<int>(m)); 
-        for (int i=0; i<n; ++i) {
-            if (nums1[i] == nums2[0]) {
-                dp[i][0] = 1; 
-                ans = max(ans , dp[i][0]); 
-            }
-        }
-        for (int i=0; i<m; ++i) {
-            if (nums2[i] == nums1[0]) {
-                dp[0][i] = 1; 
-                ans = max(ans , dp[0][i]); 
-            }
-        }
-        for (int i=1; i<n; ++i) {
-            for (int j=1; j<m; ++j) {
+        vector<vector<int>>dp(n+1 , vector<int>(m+1)); 
+        for (int i=n-1; i>=0; --i) {
+            for (int j=m-1; j>=0; --j) {
                 if (nums1[i] == nums2[j]) {
-                    dp[i][j] = dp[i-1][j-1] + 1; 
-                } else {
-                    dp[i][j] = 0; 
+                    dp[i][j] = dp[i+1][j+1] + 1; 
                 }
                 ans= max(ans , dp[i][j]); 
             }
