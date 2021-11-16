9e6524758c4df2bb1596852d0099b0e6c11e5acd
offamitkumar <offamitkumar@gmail.com>
2021-11-16 22:02:21 +0530

    factorial, maths

--- Problems/60. Permutation Sequence.cpp
+++ Problems/60. Permutation Sequence.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        string res{}; 
-        for (int i=0; i<n; ++i) {
-            res+=to_string(i+1); 
+        vector<int>num; 
+        int fact{1}; 
+        for(int i=1; i<n; ++i) {
+            fact*=i; 
+            num.push_back(i); 
         }
-        while (--k) {
-            next_permutation(res.begin(), res.end()); 
+        num.push_back(n); 
+        string ans{}; 
+        k-=1; 
+        while(true) {
+            ans += to_string(num.at(k/fact)); 
+            num.erase(num.begin()+k/fact); 
+            if (num.size() == 0) {
+                break;
+            }
+            k%=fact; 
+            fact/=num.size(); 
         }
-        return res; 
+        return ans; 
     }
 };
