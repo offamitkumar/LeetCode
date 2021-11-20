985f87a1bb53518f32971758e05f9a23023698a6
offamitkumar <offamitkumar@gmail.com>
2021-11-20 10:01:11 +0530

    binary search

--- Problems/540.cpp
+++ Problems/540.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& arr) {
-        int start = 0 ,end = arr.size()-1;
-        while(start < end){
-            int mid = (start + end)/2;
-            if(mid&1){
-                if(arr[mid] == arr[mid-1]){
-                    start = mid+1;
-                }else{
-                    end = mid-1;
+        int low = 0, high = arr.size()-1; 
+        while (low < high) {
+            int mid = (high + low) / 2; 
+            if (mid&1) {
+                if (arr[mid] == arr[mid-1]) {
+                    low = mid + 1; 
+                } else {
+                    high = mid -1; 
                 }
-            }else{
-                if(arr[mid] == arr[mid+1]){
-                    start = mid+2;
-                }else{
-                    end = mid;
+            }else {
+                if (arr[mid] == arr[mid+1]) {
+                    low = mid + 2; 
+                } else {
+                    high = mid; 
                 }
             }
         }
-        return arr[start];
+        return arr[low]; 
     }
 };
