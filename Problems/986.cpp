ade99b571dcec6235016a08ee106d4e15501c910
offamitkumar <offamitkumar@gmail.com>
2021-11-24 11:27:29 +0530

    two pointer

--- Problems/986.cpp
+++ Problems/986.cpp
@@ -1,31 +1,21 @@
 class Solution {
-    vector<int> intersection(vector<int>&interval_first, vector<int>&interval_second){
-        vector<int>res;
-        int left_point = max(interval_first[0] , interval_second[0]);
-        int right_point = min(interval_first[1] , interval_second[1]);
-        if(left_point>right_point){
-            res.push_back(-1);
-        }else{
-            res.push_back(left_point);
-            res.push_back(right_point);
-        }
-        return res;
-    }
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<vector<int>>ans;
-        int i=0 , j = 0;
-        while(i<A.size() and j<B.size()){
-            vector<int>temp = intersection(A[i] , B[j]);
-            if(temp[0] != -1)
-                ans.push_back(temp);
-            if(A[i][1]>B[j][1]){
-                ++j;
-            }else{
-                ++i;
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>>ans; 
+        int i{}, j{}; 
+        while (i<firstList.size() && j<secondList.size()) {
+            int first = max(firstList[i][0] , secondList[j][0]); 
+            int second = min(firstList[i][1], secondList[j][1]); 
+            if (first <= second) {
+                ans.push_back({first, second}); 
+            } 
+            if (firstList[i][1] < secondList[j][1]) {
+                ++i; 
+            } else {
+                ++j; 
             }
         }
-        return ans;
+        return ans; 
     }
 };
 
