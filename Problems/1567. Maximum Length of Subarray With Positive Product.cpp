4734f239f30513095decd8d9df1945b0090f370c
offamitkumar <offamitkumar@gmail.com>
2021-12-02 00:17:32 +0530

    dp, constant space

--- Problems/1567. Maximum Length of Subarray With Positive Product.cpp
+++ Problems/1567. Maximum Length of Subarray With Positive Product.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int getMaxLen(vector<int>& nums) {
         const int &n = nums.size(); 
-        vector<int>pos(n),neg(n); 
-        pos[0] = (nums[0] > 0); 
-        neg[0] = (nums[0] < 0); 
-        int ans = pos[0];
+        int pos = (nums[0] > 0); 
+        int neg = (nums[0] < 0); 
+        int ans = pos;
         for (int i=1; i<n; ++i) {
             if (nums[i] > 0) {
-                pos[i] = pos[i-1]+1; 
-                neg[i] = (neg[i-1]>0) ? 1 + neg[i-1] : 0; 
+                pos += 1; 
+                neg += (neg>0) ? 1 : 0; 
             } else if (nums[i] < 0) {
-                pos[i] = (neg[i-1]>0) ? 1 + neg[i-1] : 0; 
-                neg[i] = pos[i-1] + 1; 
+                int temp = pos; 
+                pos = (neg>0) ? 1 + neg : 0; 
+                neg = temp + 1; 
+            } else {
+                pos = neg = 0; 
             }
-            ans = max(ans , pos[i]);
+            ans = max(ans , pos);
         }
         return ans; 
     }
