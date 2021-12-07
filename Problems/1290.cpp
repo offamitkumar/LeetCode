8220e98a25676a165cd5d02fc0b55ea2e1c00321
offamitkumar <offamitkumar@gmail.com>
2021-12-07 09:43:21 +0530

    bitmanipulation

--- Problems/1290.cpp
+++ Problems/1290.cpp
@@ -8,27 +8,13 @@
  */
 class Solution {
 public:
-    int power(int n , int i){
-        int x = 1;
-        for(int m=0;m<=i;++m){
-            x*=2;
-        }
-        cout<<"power = "<<x<<endl;
-        return x;
-    }
     int getDecimalValue(ListNode* head) {
-        ListNode *temp=head;
-        int len =0;
-        while(temp!=NULL){
-            len++;
-            temp=temp->next;
+        int res{}; 
+        while (head != nullptr) {
+            res <<= 1; 
+            res += head -> val; 
+            head = head -> next; 
         }
-        int x=0;
-        --len;
-        while(head!=NULL){
-            x+=power(2,--len)*head->val;
-            head=head->next;
-        }
-        return x;
+        return res; 
     }
 };
