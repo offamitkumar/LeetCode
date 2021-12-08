da066a4568b099021116a1788e6d2409f0b5d99e
offamitkumar <offamitkumar@gmail.com>
2021-12-08 11:58:46 +0530

    dp, backtracking

--- Problems/416.cpp
+++ Problems/416.cpp
@@ -1,30 +1,28 @@
-
 class Solution {
     unordered_map<int,bool>dp;
-    bool dfs(vector<int>&n , int index , int target){
-        if(dp.find(target)!=dp.end()){
-            return dp[target];
+    int n; 
+    bool backtrack(vector<int>&nums, int index, int target) {
+        if (dp.find(target) != dp.end()) {
+            return dp[target]; 
         }
-        if(index>=n.size() or target<0)return false;
-        dp[target] = false;
-        for(auto i=index;i<(int)n.size(); ++i){
-            if(dfs(n , i+1 , target - n[i])){
-                dp[target] = true;
-                return dp[target];
+        if (target<0 || index >= n) {
+            return false; 
+        }
+        dp[target] = false; 
+        for (int i=index; i<n; ++i) {
+            if (backtrack(nums, i+1, target-nums[i])) {
+                dp[target] = true; 
+                break; 
             }
         }
-        return dp[target];
+        return dp[target]; 
     }
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin() , nums.end() , 0);
-        if(sum%2 or nums.size() == 1){
-            return false;
-        }else if(nums.size()==0){
-            return true;
-        }
-        sort(nums.begin() , nums.end());
-        dp[0] = true;
-        return dfs(nums , 0 , sum/2);
+        int sum = accumulate(nums.begin(), nums.end(),0); 
+        n = nums.size(); 
+        if (sum%2 or nums.size()==1) return false; 
+        dp[0] = true; 
+        return backtrack(nums, 0, sum/2); 
     }
 };
