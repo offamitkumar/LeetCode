439bdd92a6dc0f70c0de00a1a5798759ce5466dd
offamitkumar <offamitkumar@gmail.com>
2021-12-10 14:21:51 +0530

    dijkstra, binary search, bfs

--- Problems/1631. Path With Minimum Effort.cpp
+++ Problems/1631. Path With Minimum Effort.cpp
@@ -1,49 +1,43 @@
 class Solution {
-    vector<vector<bool>>visited; 
-    int n , m; 
-    void fillGrid (void) {
-        for (auto&itr:visited){
-            fill(itr.begin(), itr.end(), false); 
-        }
-        return ;
-    }
-    const int DSIZE = 4;
-    array<int,4>X{-1,1,0,0};
-    array<int,4>Y{0,0,-1,1};
-    bool okay(int r, int c) {
-        if (r<0 || c<0 || r>=n || c>=m || visited[r][c]) {
+    int rows , cols; 
+    bool okay(int row, int col) {
+        if (row < 0 || col < 0 || row >= rows || col >= cols) {
             return false; 
         }
         return true; 
     }
-    void dfs(int row, int col,int cost , vector<vector<int>>& heights) {
-        visited[row][col] = true; 
-        for (int i=0; i<DSIZE; ++i) {
-            int r = row + X[i] ;
-            int c = col + Y[i] ; 
-            if (okay(r,c) && abs(heights[r][c] - heights[row][col])<=cost) {
-                dfs(r,c,cost,heights); 
-            }
-        }
-    }
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        n = heights.size(); 
-        m= heights[0].size(); 
-        visited.resize(n); 
-        for (auto&itr:visited)
-            itr.resize(m); 
-        int low= 0, high = 3*1e5; 
-        while (low < high) {
-            int mid= (low + high) / 2; 
-            fillGrid(); 
-            dfs(0,0,mid, heights); 
-            if (visited[n-1][m-1]==true) {
-                high = mid; 
-            } else {
-                low = mid + 1; 
+        int def_cost = 10000000;
+        rows = heights.size(); 
+        cols = heights[0].size(); 
+        const int DSIZE = 4;
+        array<int,4>X{-1,1,0,0};
+        array<int,4>Y{0,0,-1,1};
+        priority_queue<tuple<int,int,int>>pq; 
+        vector<vector<int>>cost(rows, vector<int>(cols,def_cost)); 
+        cost[0][0] = 0; 
+        pq.push(make_tuple(0,0,0)); 
+        while (!pq.empty()) {
+            int current_cost = (-1)*get<0>(pq.top()); 
+            int row = get<1>(pq.top()); 
+            int col = get<2>(pq.top()); 
+            pq.pop(); 
+            if (row == rows-1 and col == cols-1) {
+                return current_cost; 
+            }
+            for (int i=0; i<DSIZE; ++i) {
+                int r = row + X[i]; 
+                int c = col + Y[i]; 
+                if (okay(r,c)) {
+                    int maxAbs= max(current_cost, abs(heights[r][c] - heights[row][col]));
+                    if (maxAbs < cost[r][c]) {
+                        cost[r][c] = maxAbs; 
+                        pq.push(make_tuple((-1)*cost[r][c],r,c)); 
+                    }
+                }
             }
         }
-        return high; 
+        return -1; 
     }
 };
