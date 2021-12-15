94bb9cbc2d91a6cb824a3f45c57cf59c96a9f28d
offamitkumar <offamitkumar@gmail.com>
2021-12-15 11:04:13 +0530

    constant memory

--- Problems/147. Insertion Sort List.cpp
+++ Problems/147. Insertion Sort List.cpp
@@ -9,47 +9,25 @@
  * };
  */
 class Solution {
-    void print(ListNode*root) {
-        ListNode*temp = root; 
-        while (temp != nullptr) {
-            cout << temp -> val << ' ';
-            temp = temp -> next;
-        }
-        cout << endl;
-    }
-    ListNode* insert(ListNode*root, ListNode*node) {
-        ListNode*temp = root; 
-        if (temp -> val  >= node -> val) {
-            temp = new ListNode(node->val); 
-            temp->next = root;
-          //  print(temp);
-            return temp;
-        }
-        while (temp -> next != nullptr && temp ->next -> val <= node -> val) {
-            temp = temp -> next;
-        }
-        if (temp -> next == nullptr) {
-            ListNode*t = new ListNode(node->val); 
-            temp -> next = t; 
-        } else {
-            ListNode* t = new ListNode(node -> val) ;
-            t -> next = temp -> next; 
-            temp -> next = t; 
-        }
-       // print(root);
-        return root;
-    }
 public:
     ListNode* insertionSortList(ListNode* head) {
-        ListNode* root = nullptr; 
-        while (head != nullptr) {
-            if (root == nullptr) {
-                root = new ListNode(head->val);
-            } else {
-                root = insert(root , head);
-            }
-            head = head -> next;
+        if (head == nullptr) {
+            return head; 
         }
-        return root; 
+        ListNode*dummy = new ListNode(0); 
+        ListNode*prev = dummy; 
+        ListNode*next = nullptr; 
+        ListNode*current = head; 
+        while (current) {
+            next = current->next; 
+            while (prev->next != nullptr && prev->next->val < current->val) {
+                prev = prev->next; 
+            }
+            current->next = prev->next; 
+            prev->next = current; 
+            prev = dummy; 
+            current = next; 
+        }
+        return dummy->next; 
     }
 };
