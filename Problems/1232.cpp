acd3a0197ad49a6fc167d1cce44e49c68f75ebb1
offamitkumar <offamitkumar@gmail.com>
2022-03-01 09:44:39 +0530

    updated

--- Problems/1232.cpp
+++ Problems/1232.cpp
@@ -4,12 +4,16 @@ public:
         if(v.size()<=2){
             return true;
         }
-        double s = double(v[1][1]-v[0][1])/double(v[1][0]-v[0][0]);
+        double s ; 
+        if (v[1][0] != v[0][0]) 
+            s = double(v[1][1]-v[0][1])/double(v[1][0]-v[0][0]);
+        else 
+            s = INT_MAX; 
         for(int i=0;i<v.size()-1;++i){
-            if(v[i+1][0]-v[i][0]==0){
-                return false;
-            }
-            double s1 = double(v[i+1][1] - v[i][1])/double(v[i+1][0]-v[i][0]);
+            double s1; 
+            if (v[i+1][0] != v[i][0]) 
+                s1 = double(v[i+1][1] - v[i][1])/double(v[i+1][0]-v[i][0]);
+            else s1 = INT_MAX; 
             if(s != s1){
                 return false;
             }
